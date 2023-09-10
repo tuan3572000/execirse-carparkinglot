974637dffd0810c9f891fca816cef4654434fd9d
tuan3572000 <tuan0990@gmail.com>
2023-09-10 20:48:29 +0700

    Initialize project

---
+++ src/test/java/com/exercise/carparking/CarparkingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exercise.carparking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarparkingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
