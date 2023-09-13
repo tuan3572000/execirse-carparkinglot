a7cfd3733a064009507433ce8d73dff8379ca0f7
approved <tuan0990@gmail.com>
2023-09-13 23:10:40 +0700

    update database name

--- Dockerfile
+++ Dockerfile
@@ -5,15 +5,15 @@ FROM openjdk:17.0.1-jdk-slim
 WORKDIR /app
 
 # Copy the packaged JAR file into the container
-COPY target/carparking-0.0.1-SNAPSHOT.jar /app/carparking.jar
+COPY target/carparking-0.0.1-SNAPSHOT.jar /app/carpark.jar
 
 # Make port 8080 available to the world outside this container
 EXPOSE 8080
 
 # Run the JAR file when the container starts
-CMD ["java", "-jar", "carparking.jar"]
+CMD ["java", "-jar", "carpark.jar"]
 
 
 
 # Execute command below to build an image
-# docker build -t carparking:latest .
+# docker build -t carpark:latest .
--- docker/application/init.sql
+++ docker/application/init.sql
@@ -1,17 +1,17 @@
-CREATE DATABASE carparkinglot;
-\c carparkinglot
-Create TABLE car_park_location (
-    car_park_no varchar(10) PRIMARY KEY,
+create DATABASE carpark;
+\c carpark
+create table car_park_location (
+    car_park_no varchar(10) primary key,
     address varchar(100),
     longitude float,
     latitude float
 );
 
-Create TABLE car_park_availability (
+create table car_park_availability (
     car_park_no varchar(10),
     lot_type varchar(10),
     total_lots integer,
     available_lots integer,
     updated_at timestamp,
-    PRIMARY KEY(car_park_no, lot_type)
+    primary key(car_park_no, lot_type)
 );
